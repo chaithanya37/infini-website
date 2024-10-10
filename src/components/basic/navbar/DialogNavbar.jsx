818094d30e51a331563ec884f0ac874289e89c14
TejaKumar123 <inceptioncom414@gmail.com>
2024-10-10 20:40:19 +0530

    navbar responsive adding

---
+++ src/components/basic/navbar/DialogNavbar.jsx
@@ -0,0 +1,12 @@
+import { Dialog } from "@mui/material"
+
+
+const DialogNavbar = ({ open, onClose }) => {
+    return (
+        <Dialog open={open} onClose={onClose}>
+            hgjh
+        </Dialog>
+    )
+}
+
+export default DialogNavbar
--- src/components/major/home/Navbar.jsx
+++ src/components/major/home/Navbar.jsx
@@ -1,14 +1,24 @@
 import Menu from "@mui/icons-material/Menu";
 import imageasset from "../../../assets/images/imageassets"
 import { useState } from "react"
+import DialogNavbar from "../../basic/navbar/DialogNavbar";
 
 const navbar = ({ setAboutvisibility }) => {
 
 	const [index, setIndex] = useState(0);
+	const [openStatus, setOpenStatus] = useState(false);
+
+	const handleOpen = () => {
+		setOpenStatus(true);
+	}
+
+	const handleClose = () => {
+		setOpenStatus(false);
+	}
 
 	return (
-		<div className="w-full h-[65px] px-[50px] flex flex-row items-center justify-between bg-[#0b021ca1] fixed z-[5] backdrop-blur-[10px]">
-			<img src={imageasset.logo1} className="h-[55px]" />
+		<div className="w-full md:h-[65px] h-[50px] md:px-[50px] px-[10px] flex flex-row items-center justify-between bg-[#0b021ca1] fixed z-[5] backdrop-blur-[10px]">
+			<img src={imageasset.logo1} className="md:h-[55px] h-[90%]" />
 			<div className="w-[500px] h-[50px] hidden md:flex flex-row items-center justify-between relative text-[white]">
 				<div className="border w-[125px] flex items-center justify-center h-[90%] rounded-[80px] border-[white] absolute z-[-1]"
 					style={{ transform: `translatex(${index * 125}px)`, transition: "0.3s" }}
@@ -56,8 +66,9 @@ const navbar = ({ setAboutvisibility }) => {
 				</div>
 			</div>
 			<div className="w-auto h-auto block md:hidden">
-				<Menu className="text-[white]" />
+				<Menu className="text-[white]" onClick={handleOpen} />
 			</div>
+			<DialogNavbar open={openStatus} onClose={handleClose} />
 
 		</div >
 	)
