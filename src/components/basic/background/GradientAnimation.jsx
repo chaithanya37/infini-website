d12b4d578c674d64fa4f21239d28152693518e30
Narayana Teja Kumar <narayanatejakumar123@gmail.com>
2024-10-14 22:05:42 +0530

    Update GradientAnimation.jsx

--- src/components/basic/background/GradientAnimation.jsx
+++ src/components/basic/background/GradientAnimation.jsx
@@ -3,11 +3,6 @@
 const GradientAnimation = ({ children }) => {
     return (
         <div className="w-full h-full relative ">
-            <div className="absolute w-full h-full">
-                <div className="w-[30dvw] h-[30dvw] border bg-white hidden">
-
-                </div>
-            </div>
             {children}
         </div>
     )
